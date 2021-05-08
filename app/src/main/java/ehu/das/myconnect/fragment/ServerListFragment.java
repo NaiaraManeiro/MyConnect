3dc5470d797dc49cdc462511629f9ac6388fcdea
anderct105 <anderct105@gmail.com>
2021-05-08 15:51:27 +0200

    Fallos de merge solucionados

--- app/src/main/java/ehu/das/myconnect/fragment/ServerListFragment.java
+++ app/src/main/java/ehu/das/myconnect/fragment/ServerListFragment.java
@@ -48,7 +48,7 @@ public class ServerListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_server_list_reduced, container, false);
+        return inflater.inflate(R.layout.fragment_server_list, container, false);
     }
 
     @Override
