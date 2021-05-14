f4d52d172944eb9b0675bfc099bf92c06715f199
anderct105 <anderct105@gmail.com>
2021-05-14 11:55:58 +0200

    Prototipo de LoginFragment

---
+++ app/src/main/java/ehu/das/myconnect/dialog/LoginFragment.java
@@ -0,0 +1,41 @@
+package ehu.das.myconnect.dialog;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import ehu.das.myconnect.R;
+
+
+public class LoginFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Button login = getActivity().findViewById(R.id.login);
+        login.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_loginFragment_to_serverListFragment);
+        });
+    }
+}
